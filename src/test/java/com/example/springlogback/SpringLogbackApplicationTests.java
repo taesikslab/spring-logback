21265874e573b18a077bf6a1b8d300742296db0b
taesikyoo <yrns0907@gmail.com>
2020-10-04 17:54:17 +0900

    initial commit

---
+++ src/test/java/com/example/springlogback/SpringLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
